28d7f086f510b49556adab35d97c36bce5131d96
our ame <onufrienkodarina@gmail.com>
2023-12-19 21:36:59 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        customText: '#9197B3',
+      },
+    },
+  },
+  plugins: [],
+}
+
